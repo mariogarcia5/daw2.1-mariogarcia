896107e257b9e96272b47532aa74d9c1276427c1
mariogarcia5 <72387652+mariogarcia5@users.noreply.github.com>
2020-11-05 19:32:04 +0100

    Add files via upload

---
+++ adivinar-inicio.php
@@ -0,0 +1,20 @@
+<html>
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+</head>
+
+<body>
+
+<h1>JUEGO DE ADIVINAR EL NÚMERO</h1>
+
+<form action="adivinar-principal.php" method="post">
+    <h2>Jugador 1</h2>
+    <p>Introduce un número entre 1 y 100:</p>
+    <input type="number" name="oculto" />
+    <input type="submit" value="Guardar número oculto" />
+</form>
+
+</body>
+
+</html>
